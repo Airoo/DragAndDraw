965a49122a3943e583eea1d2f8fd6482c207e5bd
test <che.kt@mail.ru>
2016-07-02 15:54:56 +0700

    Draw box

---
+++ app/src/main/java/com/airo/android/draganddraw/Box.java
@@ -0,0 +1,24 @@
+package com.airo.android.draganddraw;
+
+import android.graphics.PointF;
+
+/**
+ * Created by Airo on 02.07.2016.
+ */
+public class Box {
+    private PointF mOrigin;
+    private PointF mCurrent;
+    public Box(PointF origin) {
+        mOrigin = origin;
+        mCurrent = origin;
+    }
+    public PointF getCurrent() {
+        return mCurrent;
+    }
+    public void setCurrent(PointF current) {
+        mCurrent = current;
+    }
+    public PointF getOrigin() {
+        return mOrigin;
+    }
+}
--- app/src/main/java/com/airo/android/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/airo/android/draganddraw/BoxDrawingView.java
@@ -1,16 +1,25 @@
 package com.airo.android.draganddraw;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Airo on 01.07.2016.
  */
 public class BoxDrawingView extends View {
+    private Paint mBoxPaint;
+    private Paint mBackgroundPaint;
+    private Box mCurrentBox;
+    private List<Box> mBoxen = new ArrayList<>();
     private static final String TAG = "BoxDrawingView";
     // Используется при создании представления в коде
     public BoxDrawingView(Context context) {
@@ -19,6 +28,12 @@ public class BoxDrawingView extends View {
     // Используется при заполнении представления по разметке XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    // Прямоугольники рисуются полупрозрачным красным цветом (ARGB)
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(0x22ff0000);
+    // Фон закрашивается серовато-белым цветом
+        mBackgroundPaint = new Paint();
+        mBackgroundPaint.setColor(0xfff8efe0);
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -27,19 +42,39 @@ public class BoxDrawingView extends View {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 action = "ACTION_DOWN";
+                mCurrentBox = new Box(current);
+                mBoxen.add(mCurrentBox);
                 break;
             case MotionEvent.ACTION_MOVE:
                 action = "ACTION_MOVE";
+                if (mCurrentBox != null) {
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
                 break;
             case MotionEvent.ACTION_UP:
                 action = "ACTION_UP";
+                mCurrentBox = null;
                 break;
             case MotionEvent.ACTION_CANCEL:
                 action = "ACTION_CANCEL";
+                mCurrentBox = null;
                 break;
         }
         Log.i(TAG, action + " at x=" + current.x +
                 ", y=" + current.y);
         return true;
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+    // Заполнение фона
+        canvas.drawPaint(mBackgroundPaint);
+        for (Box box : mBoxen) {
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+            canvas.drawRect(left, top, right, bottom, mBoxPaint);
+        }
     }
 }
